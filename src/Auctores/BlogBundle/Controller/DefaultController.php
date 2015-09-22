e6f25b331dbe0985ed2652890c7a2aa19277d3c6
somniator18 <chavancej@gmail.com>
2015-09-22 16:09:30 +0200

    deuxieme jour

--- src/Auctores/BlogBundle/Controller/DefaultController.php
+++ src/Auctores/BlogBundle/Controller/DefaultController.php
@@ -2,12 +2,71 @@
 
 namespace Auctores\BlogBundle\Controller;
 
+use Auctores\BlogBundle\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction($page)
     {
-        return $this->render('AuctoresBlogBundle:Default:index.html.twig');
+
+
+        $repo = $this->getDoctrine()
+            ->getRepository('AuctoresBlogBundle:Article');
+        $max = 5;
+        $total = $repo->getCount();
+
+        //Verification du bon numéro de page
+        if ($page < 1 || $page > ceil($total/$max))
+        {
+            $page = 1;
+        }
+
+        $pagination = array(
+            'page' => $page,
+            'route' => 'auctores_blog_homepage',
+            'pages_count' =>ceil($total/$max),
+            'route_params' => array()
+        );
+
+        $result= $repo
+            ->getList($page,$max);
+
+        return $this->render('AuctoresBlogBundle:Default:index.html.twig', array(
+            'listArticle'  => $result,
+            'pagination' => $pagination));
+    }
+
+    public function addArticleAction()
+    {
+        $article = new Article();
+        $article->setTitle("Le blog decade fait son test numero ");
+
+        $article->setDate(new \DateTime());
+        $article->setText("Le blog de Decade est heureux de vous accueillir sur ce site.
+        Vous pourrez y suivre l'avancée du projet ainsi que les dernières actualités concernant le concept.");
+        $article->setSlug("test");
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($article);
+        $em->flush();
+
+        $article->setTitle("Le blog decade fait son test numero ".$article->getId());
+        $article->setSlug("test".$article->getId());
+        $em->persist($article);
+        $em->flush();
+
+
+        return $this->redirect($this->generateUrl('auctores_blog_homepage'));
+
+    }
+
+    public function articleAction($slug)
+    {
+        $article= $this->getDoctrine()
+            ->getRepository('AuctoresBlogBundle:Article')->findOneBySlug($slug);
+
+        return $this->render('AuctoresBlogBundle:Default:article.html.twig', array(
+            'article'  => $article,));
     }
 }
---
+++ src/Auctores/BlogBundle/Entity/Article.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace Auctores\BlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Article
+ *
+ * @ORM\Table(name="blog_article")
+ * @ORM\Entity(repositoryClass="Auctores\BlogBundle\Entity\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=100)
+     */
+    private $title;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="text", type="text")
+     */
+    private $text;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="slug", type="string", length=10)
+     */
+    private $slug;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Article
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Article
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     *
+     * @return Article
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     *
+     * @return Article
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+}
